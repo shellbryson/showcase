2a441345c30815c1b41aeda5716a3a43f33e0f4f
Shell Bryson <shell@shellbryson.com>
2023-04-10 22:18:24 +0100

    MVP1: styled placeholder and initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.scss';
 import React from 'react';
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 
 // Layouts
--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './LandingLayout.scss';
+
 export default function LandingLayout() {
 
   return (
     <div className="App">
-      <h1>Shell Bryson</h1>
-      <p>Building...</p>
-      <Link to="about">About</Link>
+      <div className="landing-bg_one"></div>
+      <div className="landing-bg_two"></div>
+
+      <div className="landing-content">
+        <h1>Shell Bryson</h1>
+        <p>Building...</p>
+        <Link to="about">About</Link>
+      </div>
     </div>
   )
 }
