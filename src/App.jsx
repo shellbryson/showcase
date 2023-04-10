27c12ea4cbc85f1390d82a12104c08252e241c51
Shell Bryson <shell@shellbryson.com>
2023-04-10 19:44:29 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Shell Bryson</h1>
+      <p>Building...</p>
+    </div>
+  )
+}
+
+export default App
