00d97c5245d1b6237221082cb7e4d414fdf11988
Shell Bryson <shell@shellbryson.com>
2023-04-11 18:31:48 +0100

    feat: pull back portrait on hover, reorganise for cleaner CSS

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -7,15 +7,26 @@ import Logo from '../components/Logo';
 import './LandingLayout.scss';
 
 // Assets
-import Portrait from '../components/Portrait';
+import PortraitImage from '../assets/Portrait-web.png';
 
 export default function LandingLayout() {
 
   return (
-    <>
-      <div className="landing__bg_one"></div>
-      <div className="landing__bg_two"></div>
-      <Portrait />
+    <div className="landing">
+      <div className="landing__bg_base">
+        <div className="landing__bg landing__bg_1_0"></div>
+        <div className="landing__bg landing__bg_1_1"></div>
+        <div className="landing__bg landing__bg_1_2"></div>
+        <div className="landing__bg landing__bg_1_3"></div>
+        <div className="landing__bg landing__bg_2_0"></div>
+        <div className="landing__bg landing__bg_2_1"></div>
+        <div className="landing__bg landing__bg_2_2"></div>
+        <div className="landing__bg landing__bg_2_3"></div>
+      </div>
+
+      <div className="landing__portrait">
+        <img src={PortraitImage} alt="Portrait" className="landing__portrait-image" />
+      </div>
 
       <div className="landing__content">
         <h1>Shell Bryson</h1>
@@ -24,6 +35,6 @@ export default function LandingLayout() {
       <div className="landing__bottom">
         <Logo />
       </div>
-    </>
+    </div>
   )
 }
