742f955d60a9813d569314108abf0c59c1c8bbcf
Shell Bryson <shell@shellbryson.com>
2023-04-11 22:50:33 +0100

    feat: content for Writing page

--- src/pages/Writer.jsx
+++ src/pages/Writer.jsx
@@ -9,6 +9,8 @@ export default function Writer() {
   return (
     <Page>
       <h1 className="small">Writer</h1>
+      <p>Science Fiction and Fantasy writer</p>
+      <p>Founding member of <a href="https://edinburghsff.com/">Edinburgh SFF</a></p>
     </Page>
   )
 }
