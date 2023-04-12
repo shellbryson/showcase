aaa41fa3a6dee7c99aca79843696d572c79e4c4d
Shell Bryson <shell@shellbryson.com>
2023-04-12 20:55:10 +0100

    fix: shrink Zone buttons, make component clickable, improve spacing

--- src/components/ZoneButton.jsx
+++ src/components/ZoneButton.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
+// Styles
 import './ZoneButton.scss';
-
-// Contexts
-//import { appVersion } from '../contexts/AppContext';
 
 export default function ZoneButton({ settings }) {
 
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(settings.path);
+  }
+
   return (
-    <div className={`zone-button ${settings.placement} ${settings.color}`}>
+    <div className={`zone-button ${settings.placement} ${settings.color}`} onClick={ handleOnClick }>
       <div className="zone-button__content">
-        <p><Link to={settings.path}>{settings.title}</Link></p>
+        <p>{settings.title}</p>
       </div>
     </div>
   )
